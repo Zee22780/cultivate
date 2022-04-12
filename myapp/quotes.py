a552e51161add6a6ff66ee796683573727c535d8
zuri227 <zuri22780@gmail.com>
2022-04-12 16:59:57 -0400

    Add styling to flashcard.html file

--- myapp/quotes.py
+++ myapp/quotes.py
@@ -1,7 +1,7 @@
 import random
 
 def Quotes():
-  QUOTE_DICT = {'learning' : ['The capacity to learn is a gift; the ability to learn is a skill; the willingness to learn is a choice.  ~Brian Herbert', 'Consistency of effort over the long run is everything. ~Angela Duckworth', 'Learning is the only thing the mind never exhausts, never fears, and never regrets. ~Leonardo DaVinci', 'The expert at anything was once a beginner. ~Helen Hayes']}
+  QUOTE_DICT = {'learning' : ['The capacity to learn is a gift; the ability to learn is a skill; the willingness to learn is a choice. ~Brian Herbert', 'Consistency of effort over the long run is everything. ~Angela Duckworth', 'Learning is the only thing the mind never exhausts, never fears, and never regrets. ~Leonardo DaVinci', 'The expert at anything was once a beginner. ~Helen Hayes']}
 
   QUOTES = sorted(QUOTE_DICT['learning'], key=lambda k: random.random())
 
