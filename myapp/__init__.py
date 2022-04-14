a19deba49fb907c534ac7e76f46d9e8661c87446
zuri227 <zuri22780@gmail.com>
2022-04-14 11:48:14 -0400

    Write and style Instructions Page

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -20,8 +20,8 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 # set up connection to db
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/cultivate"
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/cultivate"
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
